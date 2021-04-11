1cbb4bc70843bca91023f0249a208ba27aa9dbbe
yqlbu <yqlbu@Bu.edu>
2021-04-11 12:26:09 +0800

    updated

--- src/api/test.py
+++ src/api/test.py
@@ -1,4 +1,6 @@
+from PIL import Image
 import requests
+import io
 from urllib.request import urlopen
 
 BASE = "http://127.0.0.1:5000/"
@@ -9,6 +11,11 @@ BASE = "http://127.0.0.1:5000/"
 #  meta = site.info()  # get header of the http request
 #  if meta["content-type"] in image_formats:  # check if the content-type is a image
 #      print("it is an image")
+
+#  response = requests.get(url)
+#  image_bytes = io.BytesIO(response.content)
+#  img = Image.open(image_bytes)
+#  img.show()
 
 response = requests.post(BASE + "inference", {"url": "https://baidu.com"})
 
